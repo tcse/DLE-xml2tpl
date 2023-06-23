0ecf42dc54c6c7c88f6810565600ee8a1e5b903b
Vitaly V. Chuyakov <mail@tcse-cms.com>
2023-06-23 20:05:36 +0300

    Update ymlsync.php
    добавлен параметр запуска скрипта только по паролю.

--- ymlsync.php
+++ ymlsync.php
@@ -1,21 +1,34 @@
-<?php
+<?php 
+/*
+ * Плагин для DLE xml2tpl
+ * Позволяет сохранить разметку xml шаблона в файл
+ *
+ * @author     Talik <talik@tcse-cms.com>
+ */
+if(isset($_GET['pass']) && $_GET['pass'] == '123456'){
+    // Здесь можно поместить код, который нужно выполнить при успешной авторизации
+        // Ссылка на шаблон сайта из которого хотим сохранить содержимое
+        $url = 
+        'https://tcse-cms.com/archive/xml2yml/';
+          
+        // Адрес сохранения XML файла и его имя
+        $file_name = 'yml.xml';
+          
+        // Use file_get_contents() function to get the file
+        // from url and use file_put_contents() function to
+        // save the file by using base name
+        if (file_put_contents($file_name, file_get_contents($url)))
+        {
+            echo "File downloaded successfully";
+        }
+        else
+        {
+            echo "File downloading failed.";
+        }
+    echo "<br>Авторизация прошла успешно";
+} else {
+    echo "Неверный пароль";
+}
   
-    // Ссылка на шаблон сайта из которого хотим сохранить содержимое
-    $url = 
-    'https://sitename.ru/archive/xml2yml/';
-      
-    // Адрес сохранения XML файла и его имя
-    $file_name = 'uploads/yml.xml';
-      
-    // Use file_get_contents() function to get the file
-    // from url and use file_put_contents() function to
-    // save the file by using base name
-    if (file_put_contents($file_name, file_get_contents($url)))
-    {
-        echo "File downloaded successfully";
-    }
-    else
-    {
-        echo "File downloading failed.";
-    }
+    
 ?>
